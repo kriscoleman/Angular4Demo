3f1eae0423a058b809054533288944b9101a336c
kcoleman <kcoleman@springthrough.com>
2017-10-04 21:31:51 -0400

    Implemented the Comic model, Comics Controller and a simple Comics Logic

---
+++ ComicBookStoreAPI/Models/Comic.cs
@@ -0,0 +1,20 @@
+﻿namespace ComicBookStoreAPI.Models
+{
+    /// <summary>
+    /// A Comicbook
+    /// </summary>
+    public class Comic
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public int Issue { get; set; }
+
+        public string Publisher { get; set; }
+
+        public int Quantity { get; set; }
+
+        public decimal Cost { get; set; }
+    }
+}
